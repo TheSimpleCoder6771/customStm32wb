6b01b38c26735ab166bc49cef0582c71afaf12a9
ttm-SP <s.prabhudev@ttmrail.com.au>
2025-03-18 09:36:59 +1100

    CLEAN - Format on Save.

--- Project_I1/Core/Src/main.c
+++ Project_I1/Core/Src/main.c
@@ -15,8 +15,8 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+  /* USER CODE END Header */
+  /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "memorymap.h"
 #include "usart.h"
@@ -49,8 +49,8 @@
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-void PeriphCommonClock_Config(void);
+void SystemClock_Config( void );
+void PeriphCommonClock_Config( void );
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -64,7 +64,7 @@ void PeriphCommonClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
+int main( void )
 {
 
   /* USER CODE BEGIN 1 */
@@ -76,10 +76,10 @@ int main(void)
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 
   /* System interrupt init*/
-  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+  NVIC_SetPriorityGrouping( NVIC_PRIORITYGROUP_4 );
 
   /* SysTick_IRQn interrupt configuration */
-  NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),15, 0));
+  NVIC_SetPriority( SysTick_IRQn, NVIC_EncodePriority( NVIC_GetPriorityGrouping(), 15, 0 ) );
 
   /* USER CODE BEGIN Init */
 
@@ -104,7 +104,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+  while ( 1 )
   {
     /* USER CODE END WHILE */
 
@@ -117,69 +117,69 @@ int main(void)
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
+void SystemClock_Config( void )
 {
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
-  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_3)
+  LL_FLASH_SetLatency( LL_FLASH_LATENCY_3 );
+  while ( LL_FLASH_GetLatency() != LL_FLASH_LATENCY_3 )
   {
   }
 
   /* HSE configuration and activation */
   LL_RCC_HSE_Enable();
-  while(LL_RCC_HSE_IsReady() != 1)
+  while ( LL_RCC_HSE_IsReady() != 1 )
   {
   }
 
   /* HSI configuration and activation */
   LL_RCC_HSI_Enable();
-  while(LL_RCC_HSI_IsReady() != 1)
+  while ( LL_RCC_HSI_IsReady() != 1 )
   {
   }
 
   /* Main PLL configuration and activation */
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_2, 8, LL_RCC_PLLR_DIV_2);
+  LL_RCC_PLL_ConfigDomain_SYS( LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_2, 8, LL_RCC_PLLR_DIV_2 );
   LL_RCC_PLL_Enable();
   LL_RCC_PLL_EnableDomain_SYS();
-  while(LL_RCC_PLL_IsReady() != 1)
+  while ( LL_RCC_PLL_IsReady() != 1 )
   {
   }
 
   /* Sysclk activation on the main PLL */
   /* Set CPU1 prescaler*/
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAHBPrescaler( LL_RCC_SYSCLK_DIV_1 );
 
   /* Set CPU2 prescaler*/
-  LL_C2_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_2);
+  LL_C2_RCC_SetAHBPrescaler( LL_RCC_SYSCLK_DIV_2 );
 
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  LL_RCC_SetSysClkSource( LL_RCC_SYS_CLKSOURCE_PLL );
+  while ( LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL )
   {
   }
 
   /* Set AHB SHARED prescaler*/
-  LL_RCC_SetAHB4Prescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAHB4Prescaler( LL_RCC_SYSCLK_DIV_1 );
 
   /* Set APB1 prescaler*/
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB1Prescaler( LL_RCC_APB1_DIV_1 );
 
   /* Set APB2 prescaler*/
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_RCC_SetAPB2Prescaler( LL_RCC_APB2_DIV_1 );
 
-  LL_Init1msTick(64000000);
+  LL_Init1msTick( 64000000 );
 
   /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
-  LL_SetSystemCoreClock(64000000);
+  LL_SetSystemCoreClock( 64000000 );
 }
 
 /**
   * @brief Peripherals Common Clock Configuration
   * @retval None
   */
-void PeriphCommonClock_Config(void)
+void PeriphCommonClock_Config( void )
 {
-  LL_RCC_SetSMPSClockSource(LL_RCC_SMPS_CLKSOURCE_HSI);
-  LL_RCC_SetSMPSPrescaler(LL_RCC_SMPS_DIV_1);
-  LL_RCC_SetRFWKPClockSource(LL_RCC_RFWKP_CLKSOURCE_NONE);
+  LL_RCC_SetSMPSClockSource( LL_RCC_SMPS_CLKSOURCE_HSI );
+  LL_RCC_SetSMPSPrescaler( LL_RCC_SMPS_DIV_1 );
+  LL_RCC_SetRFWKPClockSource( LL_RCC_RFWKP_CLKSOURCE_NONE );
   /* USER CODE BEGIN Smps */
 
   /* USER CODE END Smps */
@@ -193,12 +193,12 @@ void PeriphCommonClock_Config(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
+void Error_Handler( void )
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
+  while ( 1 )
   {
   }
   /* USER CODE END Error_Handler_Debug */
@@ -212,11 +212,11 @@ void Error_Handler(void)
   * @param  line: assert_param error line source number
   * @retval None
   */
-void assert_failed(uint8_t *file, uint32_t line)
+void assert_failed( uint8_t * file, uint32_t line )
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+     /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
